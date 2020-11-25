133b0fa53f54f39099b436f9908f8be78ed8f11e
rs98065 <rahul.sharma2@kroger.com>
2020-11-24 16:48:05 -0800

    Init

--- code/office/demos/demo/src/main/java/com/example/demo/MyProjectApplication.java
+++ code/office/demos/demo/src/main/java/com/example/demo/MyProjectApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication
+public class MyProjectApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MyProjectApplication.class, args);
     }
 
 }
